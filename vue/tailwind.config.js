e1de862753d1d3f9abbe5d336dcbe83a08fcdfa1
gideon-del <gideonchidi471@gmail.com>
2024-02-20 15:29:29 +0100

    Styles added

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [`./src/**/*.{vue,ts,html}`],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        background: "hsl(180, 52%, 96%)",
+        filterTablets: "hsl(180, 31%, 95%)",
+        darkGrayishCyan: "hsl(180, 8%, 52%)",
+        veryDarkGrayishCyan: "hsl(180, 14%, 20%)",
+      },
+      fontSize: {
+        main: "15px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
